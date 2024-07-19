6a35fa8ce071bab9b98e6b38be066a6b79356dd1
OzgeBagci <egzobagci@gmail.com>
2024-07-19 13:25:59 +0200

    React weather app moved to VS Code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WeatherSearch from "./WeatherSearch";
+import App from "./App.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1> Weather App </h1>
+      <WeatherSearch />
+    </div>
   </React.StrictMode>
 );
 
